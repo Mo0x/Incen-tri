ce9c004079464fa67331cc280013ae6481733246
xam <max.govindaraju@gmail.com>
2025-11-15 01:50:36 +0100

    init svelte

---
+++ incentry/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    let appName="Incen'tri";
+</script>
+<h1>Incen-fucking-try</h1>
+<p>future {appName} WIP</p>
+<main>
+    <h1>Hello Incentri 👋</h1>
+    <p>Strava for composting – incen-try, incen-tri.</p>
+    <p>Make composting as addictive as checking your steps.</p>
+</main>
